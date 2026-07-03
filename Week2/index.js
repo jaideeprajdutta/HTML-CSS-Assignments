4d1aa84f49ffc28709633b4c2dd93e0b01375e10
jaideeprajdutta <sjaideeprajdutta@gmail.com>
2026-07-03 14:32:37 +0530

    inproved test heatmap

---
+++ Week2/index.js
@@ -0,0 +1,18 @@
+function isLegal(age) {
+    if (age >= 18) {
+        console.log("You are legally an adult.");
+    }
+    else {
+        console.log("You are not legally an adult.");
+    }
+}
+
+for (let i = 0; i <= 18; i++) {
+    isLegal(i);
+}
+
+while (age < 18) {
+    console.log("You are not legally an adult.");
+    age++;
+}
+
